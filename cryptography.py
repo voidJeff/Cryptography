76a05bf2790ad098d53cfe05aedf087e5ba3f105
voidJeff <qy120838@126.com>
2015-09-25 15:02:08 -0400

    Updated from Brython Server: September 25, 2015 3:02:08 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jeff
+Credit: None
 
 Assignment:
 
@@ -10,3 +10,31 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+n_num = []
+new_n = []
+action = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+if action == "e":
+    message = input("Message: ")
+    key = input("Key: ")
+    
+    messagE = list(message)   #message
+    n = len(message)
+    n_num = [associations.find(x) for x in messagE]
+    
+    keY = list(key)     #key
+    m = len(key)
+    M = m
+    m_num = [associations.find(x) for x in keY]
+    
+    print(n_num)
+    print(m_num)
+    
+    while n > 0:
+        new_n.apped(n_num[-n] + m_num[-m])
+        m -= 1
+        n -= 1
+    if m == 0:
+        m = M
+        
+
+
