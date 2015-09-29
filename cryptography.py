73191ffc3b8cbf08f32fb016a1aae960188a9ffd
voidJeff <qy120838@126.com>
2015-09-29 14:58:09 -0400

    Updated from Brython Server: September 29, 2015 2:58:09 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -9,68 +9,67 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-n_num = []
-new_n = []
 action = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-if action == "e":
-    message = input("Message: ")
-    key = input("Key: ")
+while action != "q":
+    associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+    n_num = []
+    new_n = []
+    action = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if action == "e":
+        message = input("Message: ")
+        key = input("Key: ")
     
-    messagE = list(message)   #message
-    n = len(message)        #how many letters
-    n_num = [associations.find(x) for x in messagE]  #transformed into numbers
+        messagE = list(message)   #message
+        n = len(message)        #how many letters
+        n_num = [associations.find(x) for x in messagE]  #transformed into numbers
     
-    keY = list(key)     #key
-    m = len(key)        #how many
-    M = m               #a copy
-    m_num = [associations.find(x) for x in keY]   #transformed into numbers
+        keY = list(key)     #key
+        m = len(key)        #how many
+        M = m               #a copy
+        m_num = [associations.find(x) for x in keY]   #transformed into numbers
     
-    while n > 0:
-        new_num = n_num[-n] + m_num[-m]
-        while new_num > 84:
-            new_num -= 85
-        new_n.append(new_num)
-        m -= 1
-        n -= 1
-        if m == 0:
-            m = M
+        while n > 0:
+            new_num = n_num[-n] + m_num[-m]
+            while new_num > 84:
+                new_num -= 85
+            new_n.append(new_num)
+            m -= 1
+            n -= 1
+            if m == 0:
+                m = M
 
-    new_message = [associations[x] for x in new_n]
-    print("".join(new_message))
+        new_message = [associations[x] for x in new_n]
+        print("".join(new_message))
+ 
+    elif action == "d":
+        message = input("Message: ")
+        key = input("Key: ")
+    
+        messagE = list(message)   #message
+        n = len(message)        #how many letters
+        n_num = [associations.find(x) for x in messagE]  #transformed into numbers
+    
+        keY = list(key)     #key
+        m = len(key)        #how many
+        M = m               #a copy
+        m_num = [associations.find(x) for x in keY]   #transformed into numbers
+    
+        while n > 0:
+            new_num = n_num[-n] - m_num[-m]
+            while new_num < 0:
+                new_num = 85 + new_num
+            new_n.append(new_num)
+            m -= 1
+            n -= 1
+            if m == 0:
+                m = M
+             
+        new_message = [associations[x] for x in new_n]
+        print("".join(new_message))
 
-elif action == "d":
-    message = input("Message: ")
-    key = input("Key: ")
-    
-    messagE = list(message)   #message
-    n = len(message)        #how many letters
-    n_num = [associations.find(x) for x in messagE]  #transformed into numbers
-    
-    keY = list(key)     #key
-    m = len(key)        #how many
-    M = m               #a copy
-    m_num = [associations.find(x) for x in keY]   #transformed into numbers
-    
-    while n > 0:
-        new_num = n_num[-n] - m_num[-m]
-        while new_num < 0:
-            new_num = 85 + new_num
-        new_n.append(new_num)
-        m -= 1
-        n -= 1
-        if m == 0:
-            m = M
-            
-    new_message = [associations[x] for x in new_n]
-    print("".join(new_message))
-
-elif action == "q":
-    print("Goodbye!")
-    
-else:
-    print("Did not understand command, try again.")
-
+    else:
+        print("Did not understand command, try again.")
+print("Goodbye!")
 
 
 
